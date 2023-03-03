8b1c7c12108b4b916f62c608da5a409b94f9b4dc
AndoRyo <ando.ryo.di@focus-s.com>
2023-03-03 17:59:57 +0900

    kneipp

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// $(window).on('load resize', function () {
+//     var window_height = window.innerHeight ? window.innerHeight : $(window).innerHeight();
+//     var window_width = window.innerWidth ? window.innerWidth : $(window).width();
+//     if (window_width <= 768) {
+//         $('.main-visual').css('height', window_height + 'px');
+//         // $('.mv').css('height', window_height + 'px');
+//     } else {
+//         $('.main-visual').css('height', '80vh');
+//         // $('.mv').css('height', '80vh');
+//     }
+// });
